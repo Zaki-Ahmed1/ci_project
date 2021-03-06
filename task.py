e0108b68c514b7372728981294374fb915474d08
Zaki Ahmed <ZakiAhmed@alumni.usc.edu>
2021-03-05 19:32:17 -0500

    Leap year counter implemented

--- task.py
+++ task.py
@@ -22,17 +22,79 @@ def my_datetime(num_sec):
     if num_sec == 0:
         return "01-01-1970"
     
-    # year
+    year = 1970
+    day = 1
+    month = 1
+
+    year_count = 0
+    month_count = 0
+    day_count = 0
+
+    calendar = [
+        [1, 31],    # Jan
+        [1, 28],    # Feb
+        [1, 31],    # Mar
+        [1, 30],    # Apr
+        [1, 30],    # May
+        [1, 31],    # Jun
+        [1, 31],    # Jul
+        [1, 31],    # Aug
+        [1, 30],    # Sep
+        [1, 31],    # Oct
+        [1, 30],    # Nov
+        [1, 31]     # Dec
+    ]
+
+    calendar_leap = [
+        [1, 31],    # Jan
+        [1, 29],    # Feb (leap)
+        [1, 31],    # Mar
+        [1, 30],    # Apr
+        [1, 30],    # May
+        [1, 31],    # Jun
+        [1, 31],    # Jul
+        [1, 31],    # Aug
+        [1, 30],    # Sep
+        [1, 31],    # Oct
+        [1, 30],    # Nov
+        [1, 31]     # Dec
+    ]
     
-    # days = num_sec / (24*60*60) # secs/ (secs/day) --> days
-    # remainder = num_sec % (24*60*60) # sec remainder --> overflow
+    day_increment = 0
+
+    days = num_sec / (24*60*60) # secs/ (secs/day) --> days
+    remainder = num_sec % (24*60*60) / 86400 # sec remainder --> overflow
+    print("days = ", days)
+    print("remainder = ", remainder)
+    print()
+
+
+    for year in range(0, 15):
+        
+        year_count = year
+        actual_year = 1970 + year_count
+
+        print("Year Count = ", year_count)
+        print("Actual year = ", actual_year)
+
+        if actual_year % 4 == 0:
+            print(actual_year, "is a leap year")
+        print("--")
 
 
 
+        # for month in calendar:
+        #     month_count += 1
+        #     for day in range(month[0], month[1]):
     return 0
 
 
 
 # Test
 print(my_datetime(0))
-print(my_datetime(123))
+print()
+# print(my_datetime(123))
+print(my_datetime(123456789))
+
+
+
