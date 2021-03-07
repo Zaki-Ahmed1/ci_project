b49ece3a08d793891a943a85d24c605288de2419
Bryan <brod26@gmail.com>
2021-03-07 15:31:47 -0800

    Add integer, decimal logic conversion and tests
    named tests in 100s to avoid merge conflicts
    created initial logic for number conversions

--- task.py
+++ task.py
@@ -3,9 +3,26 @@
 # Class: CS 362
 # Assignment: Group Project: Part 1
 
+hexHash = {
+    "0": 0, 
+    "1": 1, 
+    "2": 2, 
+    "3": 3, 
+    "4": 4, 
+    "5": 5, 
+    "6": 6, 
+    "7": 7, 
+    "8": 8, 
+    "9": 9, 
+    "A": 10, 
+    "B": 11, 
+    "C": 12, 
+    "D": 13, 
+    "E": 14, 
+    "F": 16,
+}
 
 # Function 1
-# ! cannot use int(), float(), hex(), literal_eval()
 def conv_num(num_str):
     """This function takes in a string and converts it into a base 10 number
     and returns it.
@@ -13,5 +30,82 @@ def conv_num(num_str):
     Args:
         num_str (string): returns a base 10 int
     """
+    
+    numbertoConvert = num_str
+    isNegative = False
+    isHex = False
+    isDecimal = False
+    output = 0
+    
+    if numbertoConvert[0] == "-":
+        isNegative = True
+        numbertoConvert = numbertoConvert[1:]
+    
+    if numbertoConvert[0] == "0x":
+        isHex = True
+        numbertoConvert = numbertoConvert[2:]
+    
+    if "." in num_str:
+        isDecimal = True
+    
+    if not isHex:
+        output = numberStringtoInt(numbertoConvert, isDecimal)
+    
+    if isNegative:
+        output *= -1
+    
+    return output
 
-    pass
+def numberStringtoInt(string, isDecimal):
+    
+    output = 0
+    index = 0
+    stringtoConvert = string
+    stringLength = len(string) - 1
+    
+    if isDecimal:
+        toAdd, decimalIndex = decStringtoInt(string, isDecimal)
+        output += toAdd
+        if decimalIndex == 0:
+            return output
+        stringtoConvert = string[0:decimalIndex]
+    
+    for number in stringtoConvert:
+        output += hexHash[number] * (10**stringLength)
+        stringLength -=1
+    
+    return output
+        
+        
+def decStringtoInt(string, isDecimal):
+    
+    decimalIndex = string.index(".")
+    # numString = string[0:decimalIndex]
+    # numLength = len(numString) - 1
+    decString = string[decimalIndex:]
+    decLength = len(decString) - 1
+    output = 0
+        
+    for number in decString[1:]:
+        if number == "." and isDecimal:
+            return None
+        output += hexHash[number] * (10**decLength)
+        decLength -= 1
+
+    output = output / (10**len(decString))
+    return output, decimalIndex
+        
+    
+        # for number in numString: 
+            
+        #     if number not in hexHash.keys():
+        #         return None
+            
+        #     output += hexHash[number] * (10**numLength)
+        #     numLength -= 1
+        #     index += 1
+        
+        # return output
+    
+test = "123.45"
+print(conv_num(test))
--- tests.py
+++ tests.py
@@ -4,14 +4,36 @@
 # Assignment: Grou Project: Part 1
 
 
+from task import conv_num
 import unittest
 
 
 class TestCase(unittest.TestCase):
 
-    def test1(self):
-        self.assertTrue(True)
+    def test100(self):
+        string = "12345"
+        output = 12345
+        self.assertEqual(output, conv_num(string))
+    
+    # def test101(self):
+    #     string = "-123.45"
+    #     output = -123.45
+    #     self.assertEqual(output, conv_num(string))
 
+    # def conv_num_test1(self):
+    #     self.assertTrue(True)
+    
+    # def conv_num_test1(self):
+    #     self.assertTrue(True)
+    
+    # def conv_num_test1(self):
+    #     self.assertTrue(True)
+    
+    # def conv_num_test1(self):
+    #     self.assertTrue(True)
+    
+    # def conv_num_test1(self):
+    #     self.assertTrue(True)
 
 if __name__ == '__main__':
     unittest.main()
