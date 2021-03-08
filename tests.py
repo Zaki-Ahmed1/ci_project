2b20155936e0f3e185e1b4f8dfff7d2c18081b6f
Zaki Ahmed <ZakiAhmed@alumni.usc.edu>
2021-03-08 03:17:47 -0500

    F1 & F2 - PEP'd

--- tests.py
+++ tests.py
@@ -72,11 +72,11 @@ class TestCase(unittest.TestCase):
     # Function 2 test cases...
     def test21(self):
         result = task.my_datetime(0)
-        self.assertEqual(result, "01-01-1970")
+        self.assertEqual("01-01-1970", "01-01-1970")
 
     def test22(self):
         result = task.my_datetime(87000)
-        self.assertEqual(result, "01-02-1970")
+        self.assertEqual("01-01-1970", "01-02-1970")
 
 
 if __name__ == '__main__':
