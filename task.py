ed2c63a5c06ea2473179af09ba9dd51a0ebb0ccd
Bryan <brod26@gmail.com>
2021-03-07 16:27:46 -0800

    Lint Code
    linted task and tests.py against PEP8

--- task.py
+++ task.py
@@ -4,23 +4,24 @@
 # Assignment: Group Project: Part 1
 
 hexHash = {
-    "0": 0, 
-    "1": 1, 
-    "2": 2, 
-    "3": 3, 
-    "4": 4, 
-    "5": 5, 
-    "6": 6, 
-    "7": 7, 
-    "8": 8, 
-    "9": 9, 
-    "A": 10, 
-    "B": 11, 
-    "C": 12, 
-    "D": 13, 
-    "E": 14, 
+    "0": 0,
+    "1": 1,
+    "2": 2,
+    "3": 3,
+    "4": 4,
+    "5": 5,
+    "6": 6,
+    "7": 7,
+    "8": 8,
+    "9": 9,
+    "A": 10,
+    "B": 11,
+    "C": 12,
+    "D": 13,
+    "E": 14,
     "F": 15,
 }
+
 
 # Function 1
 def conv_num(num_str):
@@ -30,24 +31,19 @@ def conv_num(num_str):
     Args:
         num_str (string): returns a base 10 int
     """
-    
     numbertoConvert = num_str
     isNegative = False
     isHex = False
     isDecimal = False
     output = 0
-    
     if numbertoConvert[0] == "-":
         isNegative = True
         numbertoConvert = numbertoConvert[1:]
-    
     if numbertoConvert[0:2] == "0x":
         isHex = True
         numbertoConvert = numbertoConvert[2:]
-    
     if "." in num_str:
         isDecimal = True
-    
     if not isHex:
         if any(i.isalpha() for i in num_str):
             return None
@@ -55,18 +51,16 @@ def conv_num(num_str):
 
     if isHex:
         output = hexStringtoInt(numbertoConvert)
-    
     if isNegative:
         output *= -1
-    
     return output
 
+
 def numberStringtoInt(string, isDecimal):
-    
     output = 0
     stringtoConvert = string
     stringLength = len(string) - 1
-    
+
     if isDecimal:
         toAdd, decimalIndex = decStringtoInt(string, isDecimal)
         output += toAdd
@@ -74,21 +68,21 @@ def numberStringtoInt(string, isDecimal):
             return output
         stringtoConvert = string[0:decimalIndex]
         stringLength = len(stringtoConvert) - 1
-    
+
     for number in stringtoConvert:
         output += hexHash[number] * (10**stringLength)
-        stringLength -=1
-    
+        stringLength -= 1
+
     return output
-        
-        
+
+
 def decStringtoInt(string, isDecimal):
-    
+
     decimalIndex = string.index(".")
     decString = string[decimalIndex:]
     decLength = len(decString) - 1
     output = 0
-        
+
     for number in decString[1:]:
         if number == "." and isDecimal:
             return None
@@ -98,11 +92,12 @@ def decStringtoInt(string, isDecimal):
     output = output / (10**len(decString))
     return output, decimalIndex
 
+
 def hexStringtoInt(string):
-    
+
     output = 0
     stringLength = len(string) - 1
-    
+
     for hexNumber in string:
         if hexNumber not in hexHash.keys():
             return None
@@ -111,9 +106,9 @@ def hexStringtoInt(string):
             stringLength -= 1
             continue
         output += hexHash[hexNumber.upper()] * (16 ** stringLength)
-        stringLength -=1
-        
+        stringLength -= 1
+
     return output
-    
+
 test = "0xAD4"
 print(conv_num(test))
--- tests.py
+++ tests.py
@@ -1,7 +1,7 @@
 # Name: Zaki Ahmed, Bryan Rodriguez, John Tran
 # Date: 22 Feb 2021
 # Class: CS 362
-# Assignment: Grou Project: Part 1
+# Assignment: Group Project: Part 1
 
 
 from task import conv_num
@@ -14,7 +14,7 @@ class TestCase(unittest.TestCase):
         string = "12345"
         output = 12345
         self.assertEqual(output, conv_num(string))
-    
+
     def test101(self):
         string = "-123.45"
         output = -123.45
@@ -24,23 +24,22 @@ class TestCase(unittest.TestCase):
         string = ".45"
         output = 0.45
         self.assertEqual(output, conv_num(string))
-    
+
     def test103(self):
         string = "123."
         output = 123.0
         self.assertEqual(output, conv_num(string))
-    
+
     def test104(self):
         string = "0xAD4"
         output = 2772
         self.assertEqual(output, conv_num(string))
-    
+
     def test105(self):
         string = "0xAZ4"
         output = None
         self.assertEqual(output, conv_num(string))
-    
-    
+
 
 if __name__ == '__main__':
     unittest.main()
