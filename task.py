f7da81890cae21b22fc7c9f38d945e5f5108e7e1
Zaki Ahmed <ZakiAhmed@alumni.usc.edu>
2021-03-09 18:03:53 -0500

    optimizing portfolio

--- task.py
+++ task.py
@@ -1,217 +1,3 @@
-# Name: Zaki Ahmed, Bryan Rodriguez, John Tran
-# Date: 22 Feb 2021
-# Class: CS 362
-# Assignment: Group Project: Part 1
-
-
-hexHash = {
-    "0": 0,
-    "1": 1,
-    "2": 2,
-    "3": 3,
-    "4": 4,
-    "5": 5,
-    "6": 6,
-    "7": 7,
-    "8": 8,
-    "9": 9,
-    "A": 10,
-    "B": 11,
-    "C": 12,
-    "D": 13,
-    "E": 14,
-    "F": 15,
-}
-
-
-# Function 1
-def conv_num(num_str):
-    """
-    This function takes in a string and converts it into a base 10 number
-    and returns it.
-    Args: num_str (string): returns a base 10 int
-    """
-
-    if not num_str:
-        return None
-
-    if stringCleanerHelper(num_str) is None:
-        return None
-
-    # logic parameters handle if the number is negative, a hex, or a decimal
-    numbertoConvert, isNegative, isHex, isDecimal = stringCleanerHelper(
-        num_str)
-
-    if not isHex:
-        # checks if the string contains any alphabetical numbers\
-        # after being designated a non hex string
-        if any(i.isalpha() for i in num_str):
-            return None
-        # calls string integer helper function
-        output = numberStringtoInt(numbertoConvert, isDecimal)
-    if isHex:
-        # calls hex integer helper function
-        # logic below checks that only alphabetical
-        # and numeric numbers are left after parsing out
-        # hex prefix
-        if any(not i.isalpha() and not i.isnumeric()
-               for i in numbertoConvert):
-            return None
-        output = hexStringtoInt(numbertoConvert)
-
-    # makes the output
-    # negative if it was designated a "negative" string
-    if isNegative:
-        output *= -1
-    return output
-
-
-def stringCleanerHelper(string):
-    """a helper function to clean the string to be converted
-    into an integer
-
-    Args:
-        string (str): the string to be cleaned
-
-    Returns:
-        None: return None if the string is not a valid input
-        string: returns a cleaned version of the string
-        isNegative: returns a boolean if the number is negative
-        isHex: returns a boolean if the number is a hex number
-        isDecimal: return a boolean if the number has a decimal in it
-    """
-
-    numbertoConvert = string
-    isNegative = False
-    isHex = False
-    isDecimal = False
-
-    # strips the string to a "raw" version and adjusts logic parameters
-    if type(numbertoConvert) is not str:
-        return None
-    if numbertoConvert[0] == "-":
-        isNegative = True
-        if len(numbertoConvert) == 1:
-            return None
-        numbertoConvert = numbertoConvert[1:]
-    if numbertoConvert[0:2] == "0x":
-        isHex = True
-        numbertoConvert = numbertoConvert[2:]
-    if "." in numbertoConvert:
-        isDecimal = True
-
-    if any(i == " " for i in numbertoConvert) or not numbertoConvert:
-        return None
-
-    return (numbertoConvert, isNegative, isHex, isDecimal)
-
-
-def numberStringtoInt(string, isDecimal):
-    """a helper function that takes in a "cleaned"
-    string and converts the string to an integer.
-    The "cleaned" string comes form conv_num()
-
-    Args:
-        string (str): the string to be created into an integer
-        isDecimal (bool): True if the number contains a decimal,
-        used for the decimal integer helper function
-
-    Returns:
-        int: the int representation of the string passed to the function
-    """
-    output = 0
-    stringtoConvert = string
-    stringLength = len(string) - 1
-
-    # if the string is a decimal number,
-    # the number is passed into the decimal string helper
-    if isDecimal:
-        toAdd, decimalIndex = decStringtoInt(string, isDecimal)
-        # logic returns none if there is more than one decimal passed
-        if toAdd is None:
-            return None
-        output += toAdd
-        if decimalIndex == 0:
-            return output
-        # removes the string portion after
-        # the decimal on the number to be converted to an int
-        stringtoConvert = string[0:decimalIndex]
-        stringLength = len(stringtoConvert) - 1
-
-    # converts the string numbers to integers
-    for number in stringtoConvert:
-        if not number.isnumeric():
-            return None
-        output += hexHash[number] * (10**stringLength)
-        stringLength -= 1
-
-    return output
-
-
-def decStringtoInt(string, isDecimal):
-    """a helper function that takes in a "cleaned" decimal
-    string and converts the string to an integer.
-    The "cleaned" string comes form decStringtoInt
-
-    Args:
-        string (string): the decimal portion of the
-        string to create into an integer
-        isDecimal (bool): a boolean denoting the
-        string a decimal, used for logic check
-
-    Returns:
-        integer: the decimal int representation of
-        the string passed to the function
-    """
-    decimalIndex = string.index(".")
-    decString = string[decimalIndex:]
-    decLength = len(decString) - 1
-    output = 0
-
-    # converts the string decimal to an integer
-    for number in decString[1:]:
-        # checks for multiple decimals passed in the string
-        if number == "." and isDecimal:
-            return None, decimalIndex
-        output += hexHash[number] * (10**decLength)
-        decLength -= 1
-
-    # divide the output to create the decimal
-    output = output / (10**len(decString))
-    return output, decimalIndex
-
-
-def hexStringtoInt(string):
-    """a helper function that takes in a "cleaned"
-    hex string and converts it to an integer
-    The "cleaned string comes from conv_num()
-
-    Args:
-        string (string): the hex portion of the string to
-        be created into an integer
-
-    Returns:
-        int: the hex respresentation of the
-        string passed to the function
-    """
-
-    output = 0
-    stringLength = len(string) - 1
-
-    # converts the string hex to an integer
-    for hexNumber in string:
-        # checks if letters have been that are not part of the hexHash table
-        if hexNumber.upper() not in hexHash.keys():
-            return None
-        if hexNumber.isalpha():
-            # converts lower case hex values to uppercase
-            output += hexHash[hexNumber.upper()] * (16 ** stringLength)
-            stringLength -= 1
-            continue
-        output += hexHash[hexNumber] * (16 ** stringLength)
-        stringLength -= 1
-
-    return output
 
 
 # Specify num_days for normal calendar year...
@@ -267,33 +53,53 @@ def my_datetime(num_sec):
     # Initialize values for counting...
     count_day = 0
 
+
     # Iterate through each year up to max range...
     for year in range(0, 9999):
 
         # Break loop if...
+        level_1 = False
         if count_day >= num_days:
+            level_1 = True
+            print("Level 1")
             break
 
         actual_year = 1970 + year
         count_month = 0
 
+
         if is_leap_year(actual_year):
             for month in calendar_leap:
+                level_2 = False
                 if count_day >= num_days:
+                    level_2 = True
+                    print("Level 2")
                     break
 
                 count_month += 1
-                day, count_day = day_looper(month,
+                day, count_day, level_3 = day_looper(month,
                                             count_month, count_day, num_days)
 
         else:
             for month in calendar:
+                level_2 = False
                 if count_day >= num_days:
+                    level_2 = True
+                    print("Level 2")
                     break
 
                 count_month += 1
-                day, count_day = day_looper(month,
+                day, count_day, level_3 = day_looper(month,
                                             count_month, count_day, num_days)
+
+    day, count_month, actual_year = day_adjustor(level_1, level_2, level_3, day, count_month, actual_year)
+    # if level_1 == True and level_2 == True and level_3 == False:
+    #     day = 1
+    #     count_month += 1
+    # if level_1 == True and level_2 == False and level_3 == False:
+    #     day = 1
+    #     count_month = 1
+    #     actual_year += 1
 
     string = str(count_month).zfill(2) + "-" + str(day).zfill(2)
     string += "-" + str(actual_year)
@@ -320,65 +126,75 @@ def day_looper(mon, count_month, count_da, num_da):
 
     # Iterate through the days in each month whilst keeping count...
     for day in range(mon[0], mon[1] + 1):
+        # count_da += 1
+        level_3 = False
         if count_da >= num_da:
+            level_3 = True
+            print("Level 3")
             break
-
         count_da += 1
 
-    return day, count_da
+    return day, count_da, level_3
+
+def day_adjustor(level_1, level_2, level_3, day, count_month, actual_year):
+    if level_1 == True and level_2 == True and level_3 == False:
+        day = 1
+        count_month += 1
+    if level_1 == True and level_2 == False and level_3 == False:
+        day = 1
+        count_month = 1
+        actual_year += 1
+    if actual_year > 9999:
+        day = 31
+        count_month = 12
+        actual_year = 9999
+    return day, count_month, actual_year
+
+# print("Day 1 close to midnight")
+# print(my_datetime(86398))
+# print(my_datetime(86399))
+# print(my_datetime(86400), "Midnight") #midnight
+# print(my_datetime(86401))
+# print(my_datetime(86402))
+# print()
+# print("Day 2 close to midnight")
+# print(my_datetime((86400*2)-2))
+# print(my_datetime((86400*2)-1))
+# print(my_datetime(86400*2), "Midnight") # midnight
+# print(my_datetime((86400*2)+1))
+# print(my_datetime((86400*2)+2))
+# print()
+# print("Day 3 close to midnight")
+# print(my_datetime((86400*3)-2))
+# print(my_datetime((86400*3)-1))
+# print(my_datetime(86400*3), "Midnight") # midnight
+# print(my_datetime((86400*3)+1))
+# print(my_datetime((86400*3)+2))
+# print()
+# print(my_datetime(86400*29))
+# print(my_datetime(86400*30))
+# print(my_datetime(86400*31))
+# print(my_datetime(86400*32))
+# print()
+# print(my_datetime(86400*363))
+# print(my_datetime(86400*364))
+# print(my_datetime(86400*365))
+# print(my_datetime(86400*366))
+# print(my_datetime(86400*367))
+# print()
+# print(my_datetime(86400*363*5))
+# print(my_datetime(86400*364*5))
+# print(my_datetime(86400*365*5))
+# print(my_datetime(86400*366*5))
+# print(my_datetime(86400*367*5))
+# print()
 
 
-# Function 3
-def conv_endian(num, endian='big'):
-    val_num = num
-    num = abs(num)
-    array = []
-    # Check num
-    if num == 0:
-        return "00"
-    # Check if parameter endian is big and little
-    if endian != "big" and endian != "little":
-        return None
-    while num != 0:
-        # Append value of value
-        remainder = num % 16
-        num = int(num / 16)
-        if remainder > 9:
-            val = chr(ord('A') + remainder - 10)
-            array.append(val)
-        else:
-            # Append value of remainder
-            array.append(remainder)
-    # If length of array is odd, append 0
-    if len(array) % 2 == 1:
-        array.append(0)
 
-    ans = ""
-    temp = ""
-    for i in range(len(array) - 1, -1, -1):
-        temp = temp + str(array[i])
-        ans, temp = conv_endian_help(i, endian, temp, ans)
-
-    if endian == 'little':
-        # remove space when adding negative
-        ans = ans[:-1]
-        # reverse the array (doing a second reverse here)
-        ans = ans[::-1]
-    # if number less than 0 (which is negative) add symbol
-    if val_num < 0:
-        ans = "-" + ans
-    # remove the last space at the end
-    if ans[len(ans) - 1] == " ":
-        ans = ans[:len(ans) - 1]
-    return ans
+# END Testing
+# for num in range(2932100+365*2, 2933105):
+#     print("For the", str(num).zfill(2), "th day...the date is:", my_datetime(86400 * num))
 
 
-def conv_endian_help(i, endian, temp, ans):
-    if i % 2 == 0:
-        if endian == 'little':
-            temp = temp[::-1]
-        ans = ans + temp
-        ans = ans + " "
-        temp = ""
-
-    return ans, temp
+for num in range(2*350, 2*375):
+    print("For the", str(num).zfill(2), "th date...the date is:", my_datetime(86400 * num))
--- tests.py
+++ tests.py
@@ -3,100 +3,100 @@
 # Class: CS 362
 # Assignment: Group Project: Part 1
 
-from task import conv_num, conv_endian
+# from task import conv_num, conv_endian
 import unittest
 import task
 
 
 class TestCase(unittest.TestCase):
 
-    def test1(self):
-        self.assertTrue(True)
+    # def test1(self):
+    #     self.assertTrue(True)
 
-    def test100(self):
-        string = "12345"
-        output = 12345
-        self.assertEqual(output, conv_num(string))
+    # def test100(self):
+    #     string = "12345"
+    #     output = 12345
+    #     self.assertEqual(output, conv_num(string))
 
-    def test101(self):
-        string = "-123.45"
-        output = -123.45
-        self.assertEqual(output, conv_num(string))
+    # def test101(self):
+    #     string = "-123.45"
+    #     output = -123.45
+    #     self.assertEqual(output, conv_num(string))
 
-    def test102(self):
-        string = ".45"
-        output = 0.45
-        self.assertEqual(output, conv_num(string))
+    # def test102(self):
+    #     string = ".45"
+    #     output = 0.45
+    #     self.assertEqual(output, conv_num(string))
 
-    def test103(self):
-        string = "123."
-        output = 123.0
-        self.assertEqual(output, conv_num(string))
+    # def test103(self):
+    #     string = "123."
+    #     output = 123.0
+    #     self.assertEqual(output, conv_num(string))
 
-    def test104(self):
-        string = "0xAD4"
-        output = 2772
-        self.assertEqual(output, conv_num(string))
+    # def test104(self):
+    #     string = "0xAD4"
+    #     output = 2772
+    #     self.assertEqual(output, conv_num(string))
 
-    def test105(self):
-        string = "0xAZ4"
-        output = None
-        self.assertEqual(output, conv_num(string))
+    # def test105(self):
+    #     string = "0xAZ4"
+    #     output = None
+    #     self.assertEqual(output, conv_num(string))
 
-    def test106(self):
-        string = "12345A"
-        output = None
-        self.assertEqual(output, conv_num(string))
+    # def test106(self):
+    #     string = "12345A"
+    #     output = None
+    #     self.assertEqual(output, conv_num(string))
 
-    def test107(self):
-        string = "12.3.45"
-        output = None
-        self.assertEqual(output, conv_num(string))
+    # def test107(self):
+    #     string = "12.3.45"
+    #     output = None
+    #     self.assertEqual(output, conv_num(string))
 
-    def test108(self):
-        string = "100000000000000"
-        output = 100000000000000
-        self.assertEqual(output, conv_num(string))
+    # def test108(self):
+    #     string = "100000000000000"
+    #     output = 100000000000000
+    #     self.assertEqual(output, conv_num(string))
 
-    def test109(self):
-        string = ""
-        output = None
-        self.assertEqual(output, conv_num(string))
+    # def test109(self):
+    #     string = ""
+    #     output = None
+    #     self.assertEqual(output, conv_num(string))
 
-    def test110(self):
-        string = "     "
-        output = None
-        self.assertEqual(output, conv_num(string))
+    # def test110(self):
+    #     string = "     "
+    #     output = None
+    #     self.assertEqual(output, conv_num(string))
 
-    def test111(self):
-        string = "-0x."
-        output = None
-        self.assertEqual(output, conv_num(string))
+    # def test111(self):
+    #     string = "-0x."
+    #     output = None
+    #     self.assertEqual(output, conv_num(string))
 
-    def test112(self):
-        string = "-"
-        output = None
-        self.assertEqual(output, conv_num(string))
+    # def test112(self):
+    #     string = "-"
+    #     output = None
+    #     self.assertEqual(output, conv_num(string))
 
-    def test113(self):
-        string = "."
-        output = 0.0
-        self.assertEqual(output, conv_num(string))
+    # def test113(self):
+    #     string = "."
+    #     output = 0.0
+    #     self.assertEqual(output, conv_num(string))
 
-    def test114(self):
-        string = "12-34"
-        output = None
-        self.assertEqual(output, conv_num(string))
+    # def test114(self):
+    #     string = "12-34"
+    #     output = None
+    #     self.assertEqual(output, conv_num(string))
 
-    def test115(self):
-        string = 1234
-        output = None
-        self.assertEqual(output, conv_num(string))
+    # def test115(self):
+    #     string = 1234
+    #     output = None
+    #     self.assertEqual(output, conv_num(string))
 
-    def test116(self):
-        string = "12*34"
-        output = None
-        self.assertEqual(output, conv_num(string))
+    # def test116(self):
+    #     string = "12*34"
+    #     output = None
+    #     self.assertEqual(output, conv_num(string))
 
     # Function 2 test cases...
     def test201(self):
@@ -131,99 +131,101 @@ class TestCase(unittest.TestCase):
         result = task.my_datetime(86400 * 2)
         self.assertEqual(result, "01-03-1970")
 
+
+
     # Function 3 test cases
-    def test301(self):
-        num = 954786
-        example = '0E 91 A2'
-        self.assertEqual(example, conv_endian(num, 'big'))
+    # def test301(self):
+    #     num = 954786
+    #     example = '0E 91 A2'
+    #     self.assertEqual(example, conv_endian(num, 'big'))
 
-    def test302(self):
-        num = 954786
-        example = '0E 91 A2'
-        self.assertEqual(example, conv_endian(num))
+    # def test302(self):
+    #     num = 954786
+    #     example = '0E 91 A2'
+    #     self.assertEqual(example, conv_endian(num))
 
-    def test303(self):
-        num = -954786
-        example = '-0E 91 A2'
-        self.assertEqual(example, conv_endian(num))
+    # def test303(self):
+    #     num = -954786
+    #     example = '-0E 91 A2'
+    #     self.assertEqual(example, conv_endian(num))
 
-    def test304(self):
-        num = -95
-        example = '-5F'
-        self.assertEqual(example, conv_endian(num))
+    # def test304(self):
+    #     num = -95
+    #     example = '-5F'
+    #     self.assertEqual(example, conv_endian(num))
 
-    def test305(self):
-        num = 954786
-        example = 'A2 91 0E'
-        self.assertEqual(example, conv_endian(num, 'little'))
+    # def test305(self):
+    #     num = 954786
+    #     example = 'A2 91 0E'
+    #     self.assertEqual(example, conv_endian(num, 'little'))
 
-    def test306(self):
-        num = -954786
-        example = '-A2 91 0E'
-        self.assertEqual(example, conv_endian(num, 'little'))
+    # def test306(self):
+    #     num = -954786
+    #     example = '-A2 91 0E'
+    #     self.assertEqual(example, conv_endian(num, 'little'))
 
-    def test307(self):
-        example = '-A2 91 0E'
-        self.assertEqual(example, conv_endian(num=-954786, endian='little'))
+    # def test307(self):
+    #     example = '-A2 91 0E'
+    #     self.assertEqual(example, conv_endian(num=-954786, endian='little'))
 
-    def test308(self):
-        example = None
-        self.assertEqual(example, conv_endian(num=-954786, endian='small'))
+    # def test308(self):
+    #     example = None
+    #     self.assertEqual(example, conv_endian(num=-954786, endian='small'))
 
-    def test309(self):
-        num = 954786
-        example = None
-        self.assertEqual(example, conv_endian(num, endian=''))
+    # def test309(self):
+    #     num = 954786
+    #     example = None
+    #     self.assertEqual(example, conv_endian(num, endian=''))
 
-    def test310(self):
-        num = 954786
-        example = None
-        self.assertEqual(example, conv_endian(num, 'icecream'))
+    # def test310(self):
+    #     num = 954786
+    #     example = None
+    #     self.assertEqual(example, conv_endian(num, 'icecream'))
 
-    def test311(self):
-        num = 1
-        example = '01'
-        self.assertEqual(example, conv_endian(num, 'big'))
+    # def test311(self):
+    #     num = 1
+    #     example = '01'
+    #     self.assertEqual(example, conv_endian(num, 'big'))
 
-    def test312(self):
-        num = 1
-        example = '01'
-        self.assertEqual(example, conv_endian(num, 'little'))
+    # def test312(self):
+    #     num = 1
+    #     example = '01'
+    #     self.assertEqual(example, conv_endian(num, 'little'))
 
-    def test313(self):
-        num = 1
-        example = None
-        self.assertEqual(example, conv_endian(num, 'bigfish'))
+    # def test313(self):
+    #     num = 1
+    #     example = None
+    #     self.assertEqual(example, conv_endian(num, 'bigfish'))
 
-    def test314(self):
-        num = 1
-        example = None
-        self.assertEqual(example, conv_endian(num, 'littlefish'))
+    # def test314(self):
+    #     num = 1
+    #     example = None
+    #     self.assertEqual(example, conv_endian(num, 'littlefish'))
 
-    def test315(self):
-        num = -1
-        example = '-01'
-        self.assertEqual(example, conv_endian(num, 'big'))
+    # def test315(self):
+    #     num = -1
+    #     example = '-01'
+    #     self.assertEqual(example, conv_endian(num, 'big'))
 
-    def test316(self):
-        num = -1
-        example = '-01'
-        self.assertEqual(example, conv_endian(num, 'little'))
+    # def test316(self):
+    #     num = -1
+    #     example = '-01'
+    #     self.assertEqual(example, conv_endian(num, 'little'))
 
-    def test317(self):
-        num = -1
-        example = None
-        self.assertEqual(example, conv_endian(num, 'bigfish'))
+    # def test317(self):
+    #     num = -1
+    #     example = None
+    #     self.assertEqual(example, conv_endian(num, 'bigfish'))
 
-    def test318(self):
-        num = -1
-        example = None
-        self.assertEqual(example, conv_endian(num, 'littlefish'))
+    # def test318(self):
+    #     num = -1
+    #     example = None
+    #     self.assertEqual(example, conv_endian(num, 'littlefish'))
 
-    def test319(self):
-        num = 95
-        example = '5F'
-        self.assertEqual(example, conv_endian(num))
+    # def test319(self):
+    #     num = 95
+    #     example = '5F'
+    #     self.assertEqual(example, conv_endian(num))
 
 
 if __name__ == '__main__':
